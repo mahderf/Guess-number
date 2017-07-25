6d716e3f0d93678c675f1c03f384472119550908
student <mymahder@gmail.com>
2017-07-25 16:21:12 -0400

    added a new file

--- GuessNumber.java
+++ GuessNumber.java
@@ -11,23 +11,24 @@ public class GuessNumber {
 		Random r = new Random();
 		int x = 1 + r.nextInt(90);
 
-	//System.out.println( x);
-	System.out.println("Enter your Number");
+	
+	System.out.println("Enter your Number"); // prompt user to guess number
+	
 	while (x<=90)
 	{
-Scanner sc = new Scanner(System.in);
-int guess=sc.nextInt();
+Scanner sc = new Scanner(System.in); 
+int guess=sc.nextInt(); // waits for value from user
 
-	if(guess<x)
+	if(guess<x) // check if the entered value is less than the random number
 	{
 		System.out.println("you guessed too low, try  again");
 	
 	}
-	if (guess>x)
+	if (guess>x) //checks if the entered value is is greater than the random number
 	{
 		System.out.println("you guessed too high, try again");
 	}
- if (guess==x)
+ if (guess==x) // checks if the entered result matches the random number
 {
 	System.out.println(" you guessed correct");
 }
